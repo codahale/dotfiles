25335b0f5c61ddcdd9c9ea27cf11af47be4ff7f6
Coda Hale <coda.hale@gmail.com>
2015-10-04 18:00:11 -0700

    Update Lein config.

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -5,12 +5,16 @@
                   [lein-bikeshed "0.2.0"]   ; picky linting
                   [lein-cloverage "1.0.6"]  ; code coverage
                   [lein-difftest "2.0.0"]   ; humane test output
+                  [lein-hiera "0.9.0"]      ; graphviz of dependencies
+                  [com.aphyr/prism "0.1.1"] ; continuous testing
+                  [lein-cljfmt "0.3.0"]
                   ;; [lein-nodisassemble "0.1.3"]
-                  [refactor-nrepl "1.2.0-SNAPSHOT"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]
                   [cider/cider-nrepl "0.10.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                       [slamhound "1.5.5"]
-                       [pjstadig/humane-test-output "0.7.0"]]
+        :dependencies [[com.aphyr/prism "0.1.1"]
+                       [org.clojure/tools.namespace "0.2.11"]
+                       [pjstadig/humane-test-output "0.7.0"]
+                       [slamhound "1.5.5"]]
         :injections [(require 'pjstadig.humane-test-output)
                      (pjstadig.humane-test-output/activate!)]
         :repl-options {:init (require '[clojure.tools.namespace.repl
